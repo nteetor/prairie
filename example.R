dc1ea94e9af536a28f333d2de9a19c4cd2cfa3c5
Paul Teetor <paulteetor@yahoo.com>
2015-08-11 17:40:16 -0500

    Fix HTML typo

--- example.R
+++ example.R
@@ -12,7 +12,7 @@ dull() %>%
     res %>% 
       status(200) %>% 
       headers(Connection = 'close') %>% 
-      body('<div align="center"><h1>Hello, world!</h1><p>(and all who inhabit it)</p></div')
+      body('<div align="center"><h1>Hello, world!</h1><p>(and all who inhabit it)</p></div>')
     
     # NOTE: It is not necessary to return the response object
   }) %>% 
