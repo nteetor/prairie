1a3782986255f8e3772d9cfddc4e0d285508895e
nteetor <nathanteetor@gmail.com>
2015-08-14 10:23:03 -0400

    reformated roxygen comments

--- route.R
+++ route.R
@@ -1,18 +1,21 @@
 #' Simple route class
-#'
-#' This class is a glorified container for a url and the corresponding
-#' callback function. Some handy utility functions are included as 
-#' private methods. As dull middleware begins to take shape this class 
-#' will evolve more.
 #' 
-#' @section Methods:
-#' \itemize{
-#'  \item \code{uri_equals(string)}: TRUE if the route uri equals string
-#'  \item \code{uri_matches(path)}: TRUE if the route uri, as a regular expression, matches path
-#'  \item \code{assign_callback(method, callback)}: set the callback function for a specific HTTP method
-#'  \item \code{callback_for(method)}: return the callback function for a specific HTTP method
+#' This class is a glorified container for a url and the corresponding callback
+#' function. Some handy utility functions are included as private methods. As
+#' dull middleware begins to take shape this class will evolve more.
+#' 
+#' @section Methods: 
+#' \itemize{ 
+#'  \item \code{uri_equals(string)}: TRUE if the
+#'   route uri equals string 
+#'  \item \code{uri_matches(path)}: TRUE if the route
+#'   uri, as a regular expression, matches path 
+#'  \item \code{assign_callback(method, callback)}: set the callback function for a
+#'   specific HTTP method 
+#'  \item \code{callback_for(method)}: return the callback
+#'   function for a specific HTTP method 
 #' }
-#' 
+#'   
 #' @docType class
 #' @format An R6 class object.
 #' @importFrom R6 R6Class
