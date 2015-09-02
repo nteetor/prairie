8338f9a000299327d8b6ce92da0295835a9ee2a4
nteetor <nathanteetor@gmail.com>
2015-09-02 17:16:07 -0400

    route test did not work w/ URL syntax changes

--- tests/testthat/test-route.R
+++ tests/testthat/test-route.R
@@ -4,27 +4,35 @@ context('route-class')
 test_that('route stops on missing arguments', {
   expect_error(route$new())
   expect_error(route$new('GET'))
-  expect_error(route$new('GET', '/test'))
+  expect_error(route$new('GET', '^test/$'))
 })
 
 test_that('route stops on incorrect arguments', {
-  expect_error(route$new(404, '/test', function(req, res) NULL))
+  expect_error(route$new(404, '^test/$', function(req, res) NULL))
   expect_error(route$new('GET', 404, function(req, res) NULL))
-  expect_error(route$new('GET', '/test', 404))
+  expect_error(route$new('GET', '^test/$', 404))
 })
 
 # the label of this test needs improvement
 test_that('callback function is correct', {
-  expect_error(route$new('GET', '/test', function() NULL))
-  expect_error(route$new('GET', '/test', function(req) NULL))
-  expect_error(route$new('GET', '/test', function(req, res, extra_arg) NULL))
+  expect_error(route$new('GET', '^test/$', function() NULL))
+  expect_error(route$new('GET', '^test/$', function(req) NULL))
+  expect_error(route$new('GET', '^test/$', function(req, res, extra_arg) NULL))
 })
 
 test_that('uri matching works for simple strings', {
-  route_hello <- route$new('GET', '/hello', function(req, res) NULL)
+  route_hello <- route$new('GET', '^hello/$', function(req, res) NULL)
   
-  expect_true(route_hello$uri_matches('/hello'))
-  expect_false(route_hello$uri_matches('hello'))
-  expect_false(route_hello$uri_matches('/helloo'))
-  expect_true(route_hello$uri_matches('/hello/'))
+  expect_false(route_hello$uri_matches('say/hello/'))
+  expect_false(route_hello$uri_matches('hello/world/'))
+  expect_false(route_hello$uri_matches('helloo/'))
+  
+  expect_true(route_hello$uri_matches('hello/'))
+})
+
+test_that('index uri is correctly matched', {
+  route_index <- route$new('GET', '^$', function(req, res) NULL)
+  
+  expect_true(route_index$uri_matches(''))
+  expect_true(route_index$uri_matches('/'))
 })
