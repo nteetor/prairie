20f8494869b99df760f0ef51963f1b5dc65b6a1d
nteetor <nathanteetor@gmail.com>
2015-09-05 09:47:47 -0400

    callback tests updated for redirect

--- tests/testthat/test-callback-utils.R
+++ tests/testthat/test-callback-utils.R
@@ -3,13 +3,13 @@ context('callback utility functions')
 
 test_that('all response utilities loaded', {
   loaded_callback <- load_helpers(function(req, res) NULL)
-  
+
   expect_exists <- function(name) {
     eval(bquote(
       expect_true(exists(.(name), where = environment(loaded_callback), inherits = FALSE))
     ))
   }
-  
+
   expect_exists('headers')
   expect_exists('status')
   expect_exists('body')
@@ -18,22 +18,23 @@ test_that('all response utilities loaded', {
 
 test_that('all request utilities loaded', {
   loaded_callback <- load_helpers(function(req, res) NULL)
-  
+
   expect_exists <- function(name) {
     eval(bquote(
       expect_true(exists(.(name), where = environment(loaded_callback), inherits = FALSE))
     ))
   }
-  
-  expect_exists('body')  
-  expect_exists('body.request') 
+
+  expect_exists('body')
+  expect_exists('body.request')
   expect_exists('body.response')
-  expect_exists('method')  
-  expect_exists('ip')  
-  expect_exists('port')  
-  expect_exists('host_name')  
-  expect_exists('params')  
-  expect_exists('field')  
+  expect_exists('method')
+  expect_exists('ip')
+  expect_exists('port')
+  expect_exists('host_name')
+  expect_exists('params')
+  expect_exists('field')
   expect_exists('is')
   expect_exists('send')
+  expect_exists('redirect')
 })
