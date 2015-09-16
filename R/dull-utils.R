8869c88a34967bf19c890072cad3185b195a9479
nteetor <nathanteetor@gmail.com>
2015-09-16 12:29:54 -0400

    %>% is key to this package, now including and exporting

--- R/dull-utils.R
+++ R/dull-utils.R
@@ -1,22 +1,19 @@
 #' Add routes
-#' 
+#'
 #' Add a route to a dull application object. A route is comprised of an HTTP
 #' method, a URI, and a callback function.
-#' 
+#'
 #' @param .app A \code{dull_app} R6 class object
 #' @param method An HTTP method, see section \bold{HTTP} Methods for details
 #' @param uri The route URI, treated as a regular expression, see section URIs
 #' @param callback A function of two arguments \code{request} and \code{response}, see section Callback functions for details
-#' 
+#'
 #' @section HTTP Methods:
 #' TODO
-#' 
+#'
 #' @section URIs:
 #' TODO
-#' 
-#' @section Callback functions:
-#' TODO
-#' 
+#'
 #' @name adding-routes
 NULL
 
@@ -45,10 +42,10 @@ put <- function(.app, uri, callback) {
 }
 
 #' Listen for connections
-#' 
+#'
 #' Start a dull application listening for connections on a specified host and
 #' port
-#' 
+#'
 #' @param .app A \code{dull_app} R6 class object
 #' @param host A valid IPv4 address as a string, use \sQuote{0.0.0.0} to broadcast on all addresses
 #' @param port A string or number identifying the server port to listen on
@@ -57,3 +54,10 @@ put <- function(.app, uri, callback) {
 listen <- function(.app, host, port) {
   .app$run(host, port)
 }
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname chain
+#' @usage LHS \%>\% RHS
+NULL
