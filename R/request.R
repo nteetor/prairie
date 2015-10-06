47a6d78e11c79b2ac13797cf91c1ef12f20be5ab
Nathan Teetor <nathanteetor@gmail.com>
2015-10-05 22:20:34 -0400

    part of overhaul process: added missing fields, updated intialize

--- R/request.R
+++ R/request.R
@@ -16,58 +16,79 @@
 #' @docType class
 #' @keywords internal
 #' @format An R6 class object.
+#' @importFrom assertthat assert_that
 #' @export
 #' @name request
 request <- R6::R6Class(
   'request',
   public = list(
-    method = NULL,
+    base_url = NULL, # in need of route implementation
     body = NULL,
+    cookies = NULL, # need to implement cookie parser
+    fresh = NULL, # may or may not be implemented,
+    ip = NULL, 
+    ips = NULL, # may not be implemented
+    original_url = NULL, # for now, very unecessary
+    params = NULL,
+    protocol = NULL,
+    route = NULL,
+    signed_cookies = NULL, # need to implement cookie parser
+    stale = NULL, # depends on $fresh
+    subdomains = NULL,
+    xhr = NULL, # may not be implemented
+    
+    method = NULL,
     url = NULL,
-    ip = NULL,
     port = NULL,
     host_name = NULL,
-    params = NULL,
     header_fields = NULL,
 
     initialize = function(route, http_request) {
+      self$base_url <- NULL
+      self$body <- http_request$rook.input$read_lines()
+      self$cookies <- list()
+      self$ip <- http_request$SERVER_NAME
+      self$ips <- NULL
+      self$original_url <- http_request$PATH_INFO
+      self$url <- self$original_url
+      
+      self$params <- setNames(
+        list(stringr::str_match_all(self$route$uri, self$original_url)[[1]][-1]),
+        stringr::str_match_all(strsplit(self$original_url, '/'), '\\?<\\w+>')[, 2]
+      )
+      
+      self$protocol <- http_request$rook.url_scheme # definitely check this
+      self$route <- route
+      self$signed_cookies <- NULL
+      self$stale <- NULL
+      self$subdomains <- strsplit(sub('(\\.\\w+){2}/.*$', '', self$original_url), '\\.')[[1]] # breaks on 'example.com'
+      
+      # move to private or remove      
       self$method <- http_request[['REQUEST_METHOD']]
       self$url <- http_request[['PATH_INFO']]
-      self$ip <- http_request[['SERVER_NAME']]
       self$port <- http_request[['SERVER_PORT']]
       self$host_name <- http_request[['HTTP_HOST']]
-      self$body <- http_request[['rook.input']]$read_lines()
-
-      self$header_fields <- lapply(names(http_request), function(nm) {
-        if (grepl('^HTTP_', nm)) {
-          http_request[[nm]]
-        }
-      })
-      # http_headers <- Filter(function(nm) nm %>% str_detect('^HTTP_'), names(http_request))
-
-      # self$header_fields <- mget(http_headers, http_request) %>% as.list
-
-      if (is.null(route$params)) {
-        self$params <- c()
+      
+      headers <- http_request[grep('^HTTP_', names(http_request), value = TRUE)]
+      
+      if (length(headers) == 0) {
+        self$header_fields <- list()
       } else {
-        param_names <- route$params
-        param_values <- stringr::str_match_all(self$route$uri, self$url)[[1]][-1]
-        self$params <- setNames(param_values, param_names)
+        self$header_fields <- headers
       }
-
+      
       invisible(self)
     },
 
-    get_header_field = function(field) {
-      field_formatted <- paste0('HTTP_', toupper(field))
-
-      if (!(field_formatted %in% self$header_fields)) {
-        warning(paste('request for', self$url, 'does not contain header', field))
-        return(NULL)
-      }
-
-      self$header_fields[[field_formatted]]
+    get = function(field) {
+      assert_that(is.character(field))
+      self$header_fields[[tolower(field)]]
     },
+    header = function(field) {
+      self$get(field)
+    },
+    
+    # this will become the method "is"
     has_content_type = function(type) {
       type == self$header_fields[['HTTP_CONTENT_TYPE']]
     }
