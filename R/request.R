1e6363bd0c93d983de8a3a064c97f1683bf014d2
Nathan Teetor <nathanteetor@gmail.com>
2015-12-04 22:45:47 +0000

    fixed bug in request object constructor

--- R/request.R
+++ R/request.R
@@ -1,12 +1,25 @@
 #' Get an HTTP Request
 #' 
 #' Request objects, yeah
+#' 
+#' @param envir An optional environment to create the request from.
+#' 
+#' @details
+#' 
+#' The \code{envir} argument is necessary for testing and other fringe cases.
+#' Typically a \code{request} object is implicity instantiated from the Rook
+#' request environment \code{httpuv} passes to the \code{call} function. See the
+#' \code{httpuv::startServer} documentation for more information.
 #'
 #' @export
 #' @name request
 request <- function(envir) {
   if (missing(envir)) {
-    envir <- list()
+    if (exists('__request_environment', sys.frame(1), inherits = FALSE)) {
+      envir <- get('__request_environment', sys.frame(1))
+    } else {
+      envir <- list()
+    }
   }
   request__$new(envir)
 }
