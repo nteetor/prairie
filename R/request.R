5767fecf18e134d2cb8c55e8ecedb3edf4dc702d
unknown <nathanteetor@gmail.com>
2015-12-09 18:04:30 -0500

    too scattered brained

--- R/request.R
+++ R/request.R
@@ -2,26 +2,16 @@
 #' 
 #' Request objects, yeah
 #' 
-#' @param envir An optional environment to create the request from.
-#' 
-#' @details
-#' 
-#' The \code{envir} argument is necessary for testing and other fringe cases.
-#' Typically a \code{request} object is implicity instantiated from the Rook
-#' request environment \code{httpuv} passes to the \code{call} function. See the
-#' \code{httpuv::startServer} documentation for more information.
-#'
 #' @export
 #' @name request
-request <- function(envir) {
-  if (missing(envir)) {
-    if (exists('__request_environment', sys.frame(1), inherits = FALSE)) {
-      envir <- get('__request_environment', sys.frame(1))
-    } else {
-      envir <- list()
-    }
-  }
-  request__$new(envir)
+request <- function() {
+  structure(
+    list(
+      header = list(),
+      body = ''
+    ),
+    class = c('request', 'http_message')
+  )
 }
 
 request__ <- R6::R6Class(
